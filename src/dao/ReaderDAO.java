75a49647b5928d5dff73c3b04b069a05070f79a0
YuhsiHu <374522830@qq.com>
2018-11-15 20:42:55 +0800

    修改读者信息

--- src/dao/ReaderDAO.java
+++ src/dao/ReaderDAO.java
@@ -92,6 +92,7 @@ public class ReaderDAO {
 		return flag;
 	}
 
+		
 	/**
 	 * 根据id和name删除Reader，其中开启了事务
 	 * @author zengyaoNPU
@@ -292,5 +293,6 @@ public class ReaderDAO {
 		} catch (Exception e) {
 			return null;
 		}
+
 	}
 }
